dbbc565abf9213b3e12dbb7759c358310e28e926
Nick Spain <nicholas.spain96@gmail.com>
2022-07-30 23:24:17 +1000

    Implement symbol resolution

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
-use std::{error::Error, fmt::Display, path::PathBuf};
+use std::{
+    collections::{BTreeSet, HashMap, HashSet},
+    error::Error,
+    fmt::{Debug, Display},
+    fs::Permissions,
+    io::Cursor,
+    path::PathBuf,
+};
 
-use nicks_linker::{
+use byteorder::{BigEndian, ReadBytesExt};
+use faerie::ArtifactBuilder;
+use goblin::{
+    archive,
+    mach::{
+        cputype::CPU_TYPE_ARM64,
+        header::{filetype_to_str, MH_DYLIB, MH_EXECUTE},
+        symbols::{Nlist, Nlist64},
+        MachO,
+    },
+};
+use machop::{
     linker_args::{Architecture, Args},
     tbd::{self, TbdDylib},
 };
+use target_lexicon::ArmArchitecture;
 
 #[derive(Debug)]
 #[allow(clippy::large_enum_variant)]
@@ -60,11 +79,47 @@ impl<'a> TryFrom<goblin::Object<'a>> for Object<'a> {
     }
 }
 
+enum Dylib<'a> {
+    MachO(&'a MachO<'a>),
+    Tbd(&'a tbd::TbdDylib),
+}
+
+struct Symbol<'a> {
+    name: &'a str,
+    nlist: Nlist,
+    object: Dylib<'a>,
+}
+
+impl<'a> Debug for Symbol<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Symbol")
+            // .field("name", &self.name)
+            .field("nlist", &self.nlist)
+            .field("type_str()", &self.nlist.type_str())
+            .field("is_global()", &self.nlist.is_global())
+            .field("is_weak()", &self.nlist.is_weak())
+            .field("is_undefined()", &self.nlist.is_undefined())
+            .field("is_stab()", &self.nlist.is_stab())
+            .finish()
+    }
+}
+
 fn main() {
     env_logger::init();
     let mut args = Args::from_env().unwrap();
+    args.library_search_paths
+        .append(&mut vec!["/usr/lib".into(), "/usr/local/lib".into()]);
+    // Dedupe only removes consecutive duplicates so we need to sort
+    // it first. Maybe it'd be better to just use a set?
+    args.library_search_paths.sort();
+    args.library_search_paths.dedup();
+    log::debug!("Arg: {:?}", args);
+    // args.object_files = vec![args.object_files.first().unwrap().to_owned()];
+    // args.libraries = vec![];
     let mut object_files = vec![];
-    object_files.append(&mut args.object_files);
+    // let (cpu_type, cpu_subtype) = get_arch_from_flag(&args.arch.to_string())
+    //     .unwrap_or_else(|| panic!("no arch found for {}", args.arch));
+    object_files.append(&mut args.object_files.clone());
     let library_search_paths = if let Some(ref root) = args.sys_lib_root {
         args.library_search_paths
             .iter()
@@ -96,19 +151,253 @@ fn main() {
         .unwrap();
     let objects = object_contents
         .iter()
-        .map(|object_content| Object::parse(object_content.as_slice()).map_err(|e| e.to_string()))
-        .collect::<Result<Vec<Object>, String>>()
-        .unwrap();
-    
-    log::debug!("Args: {args:?}");
+        .map(|object_content| {
+            Object::parse(object_content.as_slice())
+                .map_err(|e| e.to_string())
+                .unwrap()
+        })
+        .collect::<Vec<Object>>();
     log::debug!("Linking {} objects", objects.len());
-    log::debug!("Objects: {objects:#?}");
+    // log::debug!("Objects: {objects:#?}");
+
+    let mut dylibs = vec![];
+    let mut objs: Vec<MachO> = vec![];
+    let mut unowned_objs: Vec<&MachO> = vec![];
+
+    for (i, object) in objects.iter().enumerate() {
+        match object {
+            Object::Elf(_) => todo!(),
+            Object::PE(_) => todo!(),
+            Object::Mach(mach) => match mach {
+                goblin::mach::Mach::Fat(fat) => {
+                    let arch_position = fat
+                        .iter_arches()
+                        .position(|arch| {
+                            let arch = arch.unwrap();
+                            arch.cputype() & CPU_TYPE_ARM64 != 0
+                        })
+                        .unwrap();
+                    match fat.get(arch_position) {
+                        Ok(macho) => {
+                            if macho.is_object_file() {
+                                objs.push(macho);
+                            }
+                        }
+                        Err(goblin::error::Error::BadMagic(magic_number)) => {
+                            log::trace!("Magic number {magic_number} not handled by goblin MachO parser, falling back to our own");
+                            let mut rdr = Cursor::new(archive::MAGIC);
+                            // The magic number is 64 bits but it
+                            // seems to only be the first 32 bits of
+                            // the archive's magic number.
+                            let archive_magic = rdr.read_u32::<BigEndian>().unwrap() as u64;
+                            if magic_number == archive_magic {
+                                log::trace!("Entry is an archive");
+                                let content = &object_contents[i];
+                                let arch = fat.iter_arches().nth(arch_position).unwrap().unwrap();
+                                let start = arch.offset as usize;
+                                let end = (arch.offset + arch.size) as usize;
+                                let bytes = &content[start..end];
+                                let archive = archive::Archive::parse(bytes).unwrap();
+                                for member_name in archive.members() {
+                                    let member_bytes = archive.extract(member_name, bytes).unwrap();
+                                    let macho = MachO::parse(member_bytes, 0).unwrap();
+                                    if macho.is_object_file() {
+                                        objs.push(macho);
+                                    }
+                                }
+                            }
+                        }
+                        Err(e) => panic!("{}", e),
+                    }
+                }
+                goblin::mach::Mach::Binary(macho) => {
+                    if macho.is_object_file() {
+                        unowned_objs.push(macho);
+                    } else {
+                        match macho.header.filetype {
+                            MH_EXECUTE | MH_DYLIB => dylibs.push(Dylib::MachO(macho)),
+                            _ => panic!(
+                                "unhandled macho filetype {}",
+                                filetype_to_str(macho.header.filetype)
+                            ),
+                        }
+                    }
+                }
+            },
+            Object::Archive(archive) => {
+                let bytes = &object_contents[i];
+                for member_name in archive.members() {
+                    let member_bytes = archive.extract(member_name, bytes).unwrap();
+                    let macho = MachO::parse(member_bytes, 0).unwrap();
+                    if macho.is_object_file() {
+                        objs.push(macho);
+                    }
+                }
+            }
+            Object::Tbd(tbd) => dylibs.push(Dylib::Tbd(tbd)),
+        }
+    }
+
+    // let mut executable = ArtifactBuilder::new(target_lexicon::Triple {
+    //     architecture: target_lexicon::Architecture::Arm(ArmArchitecture::Arm),
+    //     vendor: target_lexicon::Vendor::Unknown,
+    //     operating_system: target_lexicon::OperatingSystem::Unknown,
+    //     environment: target_lexicon::Environment::Unknown,
+    //     binary_format: target_lexicon::BinaryFormat::Macho,
+    // })
+    // .name(
+    //     args.output_file
+    //         .file_name()
+    //         .unwrap()
+    //         .to_str()
+    //         .unwrap()
+    //         .to_string(),
+    // )
+    // .finish();
+
+    let mut symbols: HashMap<String, Symbol> = HashMap::new();
+    let mut undefined_symbols: HashSet<String> = HashSet::new();
+
+    for obj in &objs {
+        for symbol in obj.symbols() {
+            let (name, nlist) = symbol.unwrap();
+            // println!(
+            //     "{}:\t{:?}, type={}, global={}, weak={}, undefined={}, stab={}",
+            //     name,
+            //     Nlist64::from(nlist.clone()),
+            //     nlist.type_str(),
+            //     nlist.is_global(),
+            //     nlist.is_weak(),
+            //     nlist.is_undefined(),
+            //     nlist.is_stab(),
+            // );
+            let symbol = Symbol {
+                nlist,
+                object: Dylib::MachO(obj),
+                name,
+            };
+
+            // Keep track of undefined symbols so that we can check
+            // them at the end. If we encounter a definition of the
+            // symbol it'll be removed from the set.
+            if symbol.nlist.is_undefined() {
+                undefined_symbols.insert(name.to_string());
+                continue;
+            }
+
+            // Insert the symbol, whatever is, if we've never seen it
+            // before. Otherwise, only insert it if the new symbol is
+            // not weak. If there are only weak symbols then we just
+            // take the first one.
+            //
+            // Having two "strong" symbols is not allowed (through we
+            // don't return an error - maybe we should?).
+            if let Some(existing_symbol) = symbols.get(name) {
+                if existing_symbol.nlist.is_weak() && !symbol.nlist.is_weak() {
+                    // The old symbol was weak but this one isn't - replace it.
+                    symbols.insert(name.to_string(), symbol);
+                    undefined_symbols.remove(name);
+                } else if !existing_symbol.nlist.is_weak() && !symbol.nlist.is_weak() {
+                    log::warn!(
+                        "Non-weak symbol {} already exists. Ignoring it but this is malformed.\nHave={:?}\ngot={:?}",
+                        name,
+                        existing_symbol,
+                        symbol
+                    )
+                } else {
+                    log::trace!("Weak symbol {} already seen, ignoring it", name)
+                }
+            } else {
+                symbols.insert(name.to_string(), symbol);
+                undefined_symbols.remove(name);
+            }
+        }
+    }
+
+    for obj in unowned_objs {
+        for symbol in obj.symbols() {
+            let (name, nlist) = symbol.unwrap();
+            let symbol = Symbol {
+                name,
+                nlist,
+                object: Dylib::MachO(obj),
+            };
+            if let Some(existing_symbol) = symbols.get(name) {
+                if existing_symbol.nlist.is_weak() && !symbol.nlist.is_weak() {
+                    // The old symbol was weak but this one isn't - replace it.
+                    symbols.insert(name.to_string(), symbol);
+                    undefined_symbols.remove(name);
+                } else if !existing_symbol.nlist.is_weak() && !symbol.nlist.is_weak() {
+                    log::warn!(
+                        "Non-weak symbol {} already exists. Ignoring it but this is malformed.\nHave={:?}\ngot={:?}",
+                        name,
+                        existing_symbol,
+                        symbol
+                    )
+                } else {
+                    log::trace!("Weak symbol {} already seen, ignoring it", name)
+                }
+            } else {
+                symbols.insert(name.to_string(), symbol);
+                undefined_symbols.remove(name);
+            }
+        }
+    }
+
+    for dylib in dylibs {
+        match dylib {
+            Dylib::MachO(m) => {
+                for symbol in m.symbols() {
+                    let (name, nlist) = symbol.unwrap();
+                    println!(
+                        "{}:\t{:?}, type={}, global={}, weak={}, undefined={}, stab={}",
+                        name,
+                        Nlist64::from(nlist.clone()),
+                        nlist.type_str(),
+                        nlist.is_global(),
+                        nlist.is_weak(),
+                        nlist.is_undefined(),
+                        nlist.is_stab(),
+                    );
+                }
+            }
+            Dylib::Tbd(tbd) => {
+                for export in &tbd.exports {
+                    if undefined_symbols.contains(export) {
+                        log::trace!("{export} will be defined by {}", tbd.install_name.display());
+                        undefined_symbols.remove(export);
+                    }
+                }
+            }
+        }
+    }
+
+    for symbol in symbols {
+        println!("{:?}", symbol)
+    }
+
+    if !undefined_symbols.is_empty() {
+        for symbol in undefined_symbols {
+            log::error!("{symbol} is undefined")
+        }
+        std::process::exit(1)
+    }
+
+    let fh = std::fs::File::create(&args.output_file).unwrap();
+    use std::os::unix::prelude::PermissionsExt;
+    fh.set_permissions(Permissions::from_mode(0o0777)).unwrap();
+    // executable.write(fh).unwrap();
 }
 
 fn discover_library_path(locations: &[PathBuf], library_name: &str) -> Option<PathBuf> {
+    log::trace!("Discovering library {library_name}");
     let extensions = ["tbd", "dylib", "a"];
     for prefix in locations {
         for extension in extensions {
+            log::trace!(
+                "Looking for library {library_name} with extension {extension} in {}",
+                prefix.display()
+            );
             let candidate = prefix
                 .join(&format!("lib{}", library_name))
                 .with_extension(extension);
